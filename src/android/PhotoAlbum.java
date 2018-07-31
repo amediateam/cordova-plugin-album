bd3f8f1c364d7383b2a11c75290a1632e7b440c9
Amedia <info@amedia.uz>
2018-07-31 16:23:26 +0500

    Bera

--- src/android/PhotoAlbum.java
+++ src/android/PhotoAlbum.java
@@ -22,35 +22,38 @@ public class PhotoAlbum extends CordovaPlugin {
             super.initialize(cordova, webView);
 
             try{
+
                 settings = ((WebView) webView.getEngine().getView()).getSettings();
 
-            } catch (Exception error){
+            }catch (Exception error){
+
                 settings = null;
+
             }
         }
 
         @Override
         public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-            try {
-                if (action.equals("set")) {
-                    String text = args.getString(0);
-                    settings.setUserAgentString(text);
-                    callbackContext.success(settings.getUserAgentString());
-                    return true;
-                } else if (action.equals("get")) {
-                    callbackContext.success(settings.getUserAgentString());
-                    return true;
-                } else if (action.equals("reset")) {
-                    settings.setUserAgentString(null);
-                    callbackContext.success(settings.getUserAgentString());
-                    return true;
-                }
-                callbackContext.error("Invalid action");
-                return false;
-            } catch (Exception e) {
-                callbackContext.error(e.getMessage());
-                return false;
-            }
-	    }
+               try {
+                  if (action.equals("set")) {
+                     String text = args.getString(0);
+                     settings.setUserAgentString(text);
+                     callbackContext.success(settings.getUserAgentString());
+                     return true;
+                   } else if (action.equals("get")) {
+                     callbackContext.success(settings.getUserAgentString());
+                     return true;
+                   } else if (action.equals("reset")) {
+                     settings.setUserAgentString(null);
+                     callbackContext.success(settings.getUserAgentString());
+                     return true;
+                  }
+                  callbackContext.error("Invalid action");
+                  return false;
+                } catch (Exception e) {
+                  callbackContext.error(e.getMessage());
+                  return false;
+               }
+	}
 
 }
